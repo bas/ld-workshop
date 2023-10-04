d542c69f4b674c64bb63d7760c256b58de36c55a
Bas Peters <bas@users.noreply.github.com>
2023-10-04 20:08:39 +0000

    Workshop done

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ let id = getUserId();
 
 (async () => {
   const LDProvider = await asyncWithLDProvider({
-    clientSideID: '6368836a8ea04411859bca37',
+    clientSideID: '651c21956a48da12bffe782b',
     user: {
       key: id,
       custom: {
